44c1cbf5e834937dd2f4581a82cb2be26d0fef17
goofy91 <ciaociao3191@hotmail.it>
2013-06-10 18:46:57 +0200

    fixed bugs

--- main.cpp
+++ main.cpp
@@ -138,6 +138,7 @@ void testFileSystem03() {
     char a;
     iscanf("%c", &a);
     FileSystem& fs = FileSystem::instance();
+ 
     int r = fs.unlink("ciaociao123.jpg");
     iprintf("\n\nciaociao123.jpg Valore Ritorno:  %x    %d", r, r);
 
@@ -171,18 +172,54 @@ void testDelete(){
 
 }
 
-int main() {
-    //testDelete();
-    File& file=File::instance();
-    FileSystem fs=FileSystem::instance();
-    iprintf("\nTest to write a file into the FS!!");
+void testFile01(){
+    iprintf("\nStarting the file System..Press a key to continue");
     char a;
     iscanf("%c", &a);
-    
-    int fd=fs.open("FILE02.txt",O_CREAT,16);
-    file.write(fd,"Tutto bene dai!!",16);
-    
-    memDump((char *) 0x08080000, 512);
+    iprintf("\n\n\n");
+    FileSystem fs=FileSystem::instance();
+           FlashDriver& fd = FlashDriver::instance();
+           if(a=='r'){
+    iprintf("\nStart erasing...\n");
+     if(fd.erase(8)==true)
+         iprintf("\nErasing done with success\n");
+     else{
+         iprintf("\nErasing failed");
+         return;
+     }
+           }
+    {
+    int fd=fs.open("File01",O_CREAT | O_WRONLY,80);
+    File* file=new File(fd,80);
+    file->write(fd,"Contenuto",9);
+    }
+    {
+        int fd=fs.open("File02",O_CREAT | O_WRONLY,80);
+    File* file=new File(fd,80);
+    file->write(fd,"Ciao, questo è il mio file",27);
+    }
+    {
+        int fd=fs.open("File03",O_CREAT | O_WRONLY,80);
+    File* file=new File(fd,80);
+    file->write(fd,"CIAOCIAO",8);
+    }
+    iprintf("\n\n Before unlinking File02\n\n");
+    memDump((char *) 0x08080000, 1024);
+    int ret=-1;
+    if(a=='2')
+        ret=fs.unlink("File02");
+    else if(a=='1')
+        ret=fs.unlink("File01");
+    else if(a=='3')
+        ret=fs.unlink("File03");
+    iprintf("\n\nret addr:%x\n\n",ret);
+    iprintf("\n\n After unlinking File   %c\n\n",a);
+    memDump((char *) 0x08080000, 1024); 
+}
+
+int main() {
+
+    testFile01();
 
     iprintf("\nTest done!");
     iprintf("\nTest done!");
